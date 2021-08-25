cbfd7e12047ad29c490531f9ce496c8c795320f9
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-25 18:39:33 -0300

    exercicio 2

--- exercicios-JS/script.js
+++ exercicios-JS/script.js
@@ -1,7 +1,7 @@
-const num1 = 5;
+const num1 = 2;
 const num2 = 7;
-console.log(num1+num2);
-console.log(num1-num2);
-console.log(num1*num2);
-console.log(num1/num2);
-console.log(num1%num2);
+if (num1>num2){
+    console.log(num1)
+}else{
+    console.log(num2)
+}
