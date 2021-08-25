f9b97220f43aa3f6c20d969e6163031a8a200919
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-25 19:43:57 -0300

    exercicio 5 triangulo

--- exercicios-JS/script.js
+++ exercicios-JS/script.js
@@ -1,9 +1,12 @@
-const num1 = 0;
-if (num1 >=1){
-    console.log("positive")
-}else if (num1 <= -1){
-    console.log("negative")
+const ang1 = 84;
+const ang2 = 49;
+const ang3 = 190;
+if (ang1 < (ang2+ang3)& ang2 < (ang1 + ang3)& ang3 < (ang1 +ang2)) {
+    console.log("true")
+}else if (ang1<-1 || ang2 < -1 || ang3 < -1) {
+    console.log("error")
 }else{
-    console.log("zero")
+    console.log("false")
 }
-    
+
+
