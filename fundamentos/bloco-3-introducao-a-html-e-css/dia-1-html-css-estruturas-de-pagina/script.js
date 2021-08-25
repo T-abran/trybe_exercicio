cb9086702efa996a87e771bd05987e35f5a99073
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-25 15:43:46 -0300

    mais constas java script

--- fundamentos/bloco-3-introducao-a-html-e-css/dia-1-html-css-estruturas-de-pagina/script.js
+++ fundamentos/bloco-3-introducao-a-html-e-css/dia-1-html-css-estruturas-de-pagina/script.js
@@ -1,4 +1,6 @@
 const base = 5;
 let heigth = 8;
-const area = (base * heigth);
-console.log(area)
+const area = base * heigth;
+console.log(area);
+const perimeter = heigth+heigth;
+console.log(perimeter);
