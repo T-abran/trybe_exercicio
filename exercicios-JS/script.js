0d2682275fd788ff0f677de52d16de5af7b5e61c
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-26 16:24:48 -0300

    for or

--- exercicios-JS/script.js
+++ exercicios-JS/script.js
@@ -1,5 +1,4 @@
 let almoço = ["alface","picanha","batata frita","feijao","macarrao"];
-almoço.unshift("molho branco")
-for (let index =0;index < almoço.length;index += 1 ){
-    console.log(almoço[index])
+for (let pratos of almoço){
+    console.log(pratos)
 }
