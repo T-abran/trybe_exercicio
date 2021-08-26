bc128aebf1a504d01a00c8cc243f3627999a29e7
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-26 15:26:52 -0300

    adicionado unshift

--- exercicios-JS/script.js
+++ exercicios-JS/script.js
@@ -1,5 +1,5 @@
 let almoço = ["alface","picanha","batata frita","feijao","macarrao"];
-almoço.push("molho branco")
+almoço.unshift("molho branco")
 for (let index =0;index < almoço.length;index += 1 ){
     console.log(almoço[index])
 }
