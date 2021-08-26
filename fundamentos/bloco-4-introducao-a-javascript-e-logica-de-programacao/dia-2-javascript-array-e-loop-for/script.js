a4b7aea50500ebc3d584aec39cdf08082b4aec3a
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-26 19:10:56 -0300

    tentativa exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,2 +1,5 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-console.log(numbers)
+let notas = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+    for (let i = 0; i < notas.length; i++) {
+            console.log(total += notas[i]);
+        }
