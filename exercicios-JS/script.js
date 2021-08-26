11287591a5678a74de4b2dc358d8ed0dcc60d09b
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-26 15:23:07 -0300

    array e for primeira aula

--- exercicios-JS/script.js
+++ exercicios-JS/script.js
@@ -1,12 +1,5 @@
-const ang1 = 84;
-const ang2 = 49;
-const ang3 = 190;
-if (ang1 < (ang2+ang3)& ang2 < (ang1 + ang3)& ang3 < (ang1 +ang2)) {
-    console.log("true")
-}else if (ang1<-1 || ang2 < -1 || ang3 < -1) {
-    console.log("error")
-}else{
-    console.log("false")
+let almoço = ["alface","picanha","batata frita","feijao","macarrao"];
+almoço.push("molho branco")
+for (let index =0;index < almoço.length;index += 1 ){
+    console.log(almoço[index])
 }
-
-
