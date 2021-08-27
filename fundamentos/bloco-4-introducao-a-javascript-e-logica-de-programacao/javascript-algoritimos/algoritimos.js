2d52a6c91822358b211df7f24b35d0471be1fb64
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-27 18:15:39 -0300

    symbol exercicio 1 algoritimo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-algoritimos/algoritimos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-algoritimos/algoritimos.js
@@ -1,5 +1,10 @@
-let n = [5,'*****','*****','*****'];
-for (let numeros of n ){
-    console.log(numeros)
-}
+let n = 17;
+let symbol = '*';
+let inputLine = '';
 
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};
