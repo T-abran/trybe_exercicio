9140be484daaa10331aeb5edb894ab5830063a93
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-27 18:55:38 -0300

    piramide exercicio

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-algoritimos/algoritimos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-algoritimos/algoritimos.js
@@ -2,17 +2,21 @@
 let n = 5;
 let symbol = '*';
 let inputLine = '';
-let inputPosition = n;
 
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-    if (columnIndex < inputPosition) {
-      inputLine = inputLine + ' ';
-    } else {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
       inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
     }
   }
   console.log(inputLine);
   inputLine = '';
-  inputPosition -= 1;
+  controlRight += 1;
+  controlLeft -= 1
 };
