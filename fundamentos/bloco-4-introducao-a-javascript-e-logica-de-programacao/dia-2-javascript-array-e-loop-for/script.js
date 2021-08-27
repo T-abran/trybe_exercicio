f2fbcbc117c7e1d395047c6595376ecb083b6fe3
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-27 15:59:53 -0300

    apagado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,5 +1,7 @@
 let notas = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let total = 0;
-    for (let i = 0; i < notas.length; i++) {
-            console.log(total += notas[i]);
-        }
+    for (let inicio = 0; inicio < notas.length; inicio++) {
+        total= total+notas;  
+    }
+    console.log(total)
+    
