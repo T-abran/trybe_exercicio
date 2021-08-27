1febfefb05c758db8a7f53573014bb2c74ef6480
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-27 18:28:02 -0300

    exercicio 2 algoritimo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-algoritimos/algoritimos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-algoritimos/algoritimos.js
@@ -1,10 +1,8 @@
-let n = 17;
+let size = 5;
 let symbol = '*';
 let inputLine = '';
 
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
-  inputLine = inputLine + symbol;
-};
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+for (let lineIndex = 0; lineIndex <=size; lineIndex += 1) {
   console.log(inputLine);
+  inputLine = inputLine + symbol;
 };
