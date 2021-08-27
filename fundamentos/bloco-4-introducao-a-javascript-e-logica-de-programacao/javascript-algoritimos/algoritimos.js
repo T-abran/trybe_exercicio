97bdd638361e323dc632054b491f92ccef4e2c37
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-27 17:19:19 -0300

    exercicio 1 dia de algoritimo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-algoritimos/algoritimos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-algoritimos/algoritimos.js
@@ -1,11 +1,5 @@
-let fruits = [3, 4, 10, 1, 12];
-let sum =0;
-for(let index = 0; index < fruits.length; index += 1){
-    sum += fruits[index];
+let n = [5,'*****','*****','*****'];
+for (let numeros of n ){
+    console.log(numeros)
 }
-if (sum > 15){
-    console.log(sum);
-}else{
-    console.log('valor menor que 16')
-};
 
