cf135a7bfd5750491a7ca125c5af030615e1bdd1
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-30 19:31:46 -0300

    adicionado outro objeto e imprimido juntos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios4.4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios4.4.js
@@ -4,5 +4,10 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
   info ['recorrente']='sim';
-  for (let key in info)
-  console.log(info[key])
+  let info2 = {
+      personagem:'Tio Patinhas',
+      origem:"Christmas on Bear Mountain, Dell's Four Color Comics #178",
+      nota:'O último MacPatinhas',
+    };
+  for (let key in info,info2)
+  console.log(info[key]+' '+"e"+" "+info2[key])
