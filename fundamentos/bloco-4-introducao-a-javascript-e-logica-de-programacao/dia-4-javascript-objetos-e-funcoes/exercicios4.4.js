9f3d388c38bb3d34060b4ed21ce2050b671e6fec
Tiago Abranges <88907997+T-abran@users.noreply.github.com>
2021-08-30 19:06:02 -0300

    removido o gi

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios4.4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios4.4.js
@@ -4,4 +4,4 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
   for (let key in info)
-  console.log("Bem vinda,"+" "+ info.personagem)gi
+  console.log("Bem vinda,"+" "+ info.personagem)
