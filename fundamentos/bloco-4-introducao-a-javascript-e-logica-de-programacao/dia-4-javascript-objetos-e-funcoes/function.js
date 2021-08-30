34536ff960a26ab0cb2361a7d455483af0c3c6b9
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-30 19:55:20 -0300

    invertendo array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/function.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/function.js
@@ -1,6 +1,6 @@
-let names ='tiago abranges garcia';
-resultado=names.split("")
-console.log(resultado)
+let names =['tiago','abranges','garcia'];
+names.reverse()
+console.log(names)
 /* divide uma string em um array de strings
  de acordo com algum separador; por exemplo, o espaço em 
  branco ou algo determinado por uma expressão regular. */
