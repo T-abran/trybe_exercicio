324f1edc61b6b29b60a043b69d8d29756a1ba3c6
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-30 16:12:35 -0300

    começo do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-algoritimos/algoritimos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-algoritimos/algoritimos.js
@@ -1,22 +1,7 @@
-
-let n = 5;
-let symbol = '*';
-let inputLine = '';
-
-let midOfMatrix = (n + 1) / 2;
-let controlLeft = midOfMatrix;
-let controlRight = midOfMatrix;
-
-for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
-  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-    if (columnIndex > controlLeft && columnIndex < controlRight) {
-      inputLine = inputLine + symbol;
-    } else {
-      inputLine = inputLine + ' ';
-    }
-  }
-  console.log(inputLine);
-  inputLine = '';
-  controlRight += 1;
-  controlLeft -= 1
-};
+let name = 'Marta';
+let medals = [golden= 2, silver= 3 ];
+let play ={
+    name : 'Marta',
+    medals :[golden= 2, silver= 3 ]
+}
+console.table(play)console.log("o jogador" +" " + player.name +" " +player.lastName+" " +"tem "+ player.age +" " +"anos de idade")
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/objetos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/objetos.js
@@ -6,4 +6,4 @@ let player = {
 }
 
 player['anoCampeao']=[2006, 2007, 2008, 2009, 2010, 2018];
-console.table(player)
+console.table(play)console.log("o jogador" +" " + player.name +" " +player.lastName+" " +"foi campeao "+ anoCampeao.lenght +" " +"anos de idade")
