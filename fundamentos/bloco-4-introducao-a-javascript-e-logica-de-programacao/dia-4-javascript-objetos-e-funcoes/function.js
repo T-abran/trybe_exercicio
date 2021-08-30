8dde620df126bf88e534cd18a337f5f76b1c10eb
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-30 19:50:06 -0300

    explicando split

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/function.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/function.js
@@ -1,3 +1,6 @@
 let names ='tiago abranges garcia';
 resultado=names.split("")
 console.log(resultado)
+/* divide uma string em um array de strings
+ de acordo com algum separador; por exemplo, o espaço em 
+ branco ou algo determinado por uma expressão regular. */
