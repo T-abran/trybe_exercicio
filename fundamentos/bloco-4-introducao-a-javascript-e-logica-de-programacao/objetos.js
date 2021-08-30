1bbb1b59ed929a55e5e1f2c560cc27493c7dd957
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-30 18:17:48 -0300

    deletado arquivos antigos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/objetos.js
+++
@@ -1,9 +0,0 @@
-
-let player = {
-    name : 'Tiago',
-    lastName : 'Abranges',
-    age : 20,
-}
-
-player['anoCampeao']=[2006, 2007, 2008, 2009, 2010, 2018];
-console.table(play)console.log("o jogador" +" " + player.name +" " +player.lastName+" " +"foi campeao "+ anoCampeao.lenght +" " +"anos de idade")
