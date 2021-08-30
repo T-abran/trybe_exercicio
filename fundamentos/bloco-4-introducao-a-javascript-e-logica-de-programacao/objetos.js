3a859f91957d98b6f9f23a8613dfe54aebaf0f1e
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-30 14:52:03 -0300

    primeiros passos em objetos js

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/objetos.js
@@ -0,0 +1,7 @@
+
+let player = {
+    name : 'Tiago',
+    lastName : 'Abranges',
+    age : 20,
+}
+console.log("o jogador" +" " + player.name +" " +player.lastName+" " +"tem "+ player.age +" " +"anos de idade")
