61878c9e0a351096650872672fa2a4b41625e11e
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-30 15:24:17 -0300

    primeiro a chave e depois propriedade for in

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/for-in.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/for-in.js
@@ -1,9 +1,9 @@
-let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge' 
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
   };
-  for(index in names){
-    console.log("ola"+ " " +names[index])   
+  for (index in car){
+    console.log("primeiro a sua chave:"+ " "+index," "+"e segundo sua propriedade:"+" "+car[index])
   }
   
