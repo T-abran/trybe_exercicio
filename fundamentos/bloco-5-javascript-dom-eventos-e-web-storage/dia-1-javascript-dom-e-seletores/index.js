81ba40d22c8dd0aacf0c307ab623b4f8beff0b81
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-03 19:29:33 -0300

    exercicio Dom

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("page-title").innerText = "Projeto Almanaque";/* adicionado um html no id page-tittle */
-document.getElementById("second-paragraph").innerText = "O melhor filme desse planeta";/* adicionado um html no id second-paragraph*/
-document.getElementById("subtitle").innerText = "Apenas mais um sucesso de aventura e ficção"/* adicionado html no id subtitle */
-/* let meuNome = document.getElementsByClassName("brabo");
-for (let i = 0; i < meuNome.length ; i += 1 ){
-meuNome[i].innerHTML = "tiago abranges"
-} */ /* alteração dos elementos da classe pelo nome tiago abranges, usando o getElementsByClassName e para fazer isso teve que ser criado um for para percorrer os nomes */
