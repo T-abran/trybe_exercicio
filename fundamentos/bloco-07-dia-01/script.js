872c281fa9d0b41e26899385a12a306437ec3c2d
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-24 18:56:40 -0300

    exercicio 1

--- fundamentos/bloco-07-dia-01/script.js
+++ fundamentos/bloco-07-dia-01/script.js
@@ -1,33 +1,21 @@
-// Situação em que é mais simples usar o operador ternário:
-const checkIfElse = (age) => {
+/* const checkIfElse = (age) => {
     if (age >= 18) {
       return `Você tem idade para dirigir!`;
     } else {
       return `Você ainda não tem idade para dirigir...`;
     }
-  };
-  
-  const checkTernary = (age) => (
-    age >= 18 ? `Você tem idade para dirigir!` : `Você ainda não tem idade para dirigir...`
-  );
-  
-  // ------------------------
-  
-  // Situação em que usar o operador ternário não faz muito sentido:
-  const checkIfElse = (fruit) => {
-    if (fruit === `maçã`) {
-      return `Essa fruta é vermelha`;
-    } else if (fruit === `banana`) {
-      return `Esta fruta é amarela`;
+  }; */
+
+  const testingScope= escopo =>{
+    if (escopo === true) {
+     let letras = 'tiago abranges';
+     return (`ola, ${letras}!`)
+     return juntas
     } else {
-      return `Não sei a cor`;
+        let bnome = 'brunna martins'
+     return (`olha se nao é a ${bnome}`)
     }
-  };
-  
-  const checkTernary = (fruit === `maçã`) ? `Essa fruta é vermelha` 
-    : ((fruit === `banana`) ? `Esta fruta é amarela` : `Não sei a cor`);
-  
-  // Aninhar operadores  ternários, como no exemplo acima, não é uma boa prática,
-  // pois torna o seu código truncado e difícil de ler, tirando todo o sentido de um
-  // operador cujo objetivo é facilitar a sua vida e a da pessoa que lerá seu código
-  // no futuro!
+   
+  }
+
+  console.log(testingScope(false));
