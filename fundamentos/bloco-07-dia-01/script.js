5d3033ebdfd660cf275c6b1b659a397f2efefbb1
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-24 15:33:16 -0300

    operador ternario

--- fundamentos/bloco-07-dia-01/script.js
+++ fundamentos/bloco-07-dia-01/script.js
@@ -1,27 +1,33 @@
-/* função atual */
-function printName() {
-    const myName = 'Tiago'
-    return myName
-}
-console.log(printName());
-
-/* usando arrow function */
-const printName = () => {
-    const myName = 'Tiago';
-    return myName;
-  }
-  console.log(printName());
-
-  /* Quando não há nada no corpo da função além do que será retornado, a sintaxe da arrow function nos permite simplificar ainda mais a função printName omitindo o return e as chaves: */
-  const printName = () => "Lucas";
-console.log(printName());
-
-/* Mas lembre-se que podemos omitir os parênteses apenas em um cenário:
-Quando a função recebe apenas um argumento, como podemos ver no exemplo abaixo: */
-const multiplyByTwo = number => number * 2;
-console.log(multiplyByTwo(10));
-
-/* Em funções que recebem mais de um parâmetro, os parênteses não são omitidos: */
-
-const multiplication = (number, multi) => number * multi;
-console.log(multiplication(8, 2));
+// Situação em que é mais simples usar o operador ternário:
+const checkIfElse = (age) => {
+    if (age >= 18) {
+      return `Você tem idade para dirigir!`;
+    } else {
+      return `Você ainda não tem idade para dirigir...`;
+    }
+  };
+  
+  const checkTernary = (age) => (
+    age >= 18 ? `Você tem idade para dirigir!` : `Você ainda não tem idade para dirigir...`
+  );
+  
+  // ------------------------
+  
+  // Situação em que usar o operador ternário não faz muito sentido:
+  const checkIfElse = (fruit) => {
+    if (fruit === `maçã`) {
+      return `Essa fruta é vermelha`;
+    } else if (fruit === `banana`) {
+      return `Esta fruta é amarela`;
+    } else {
+      return `Não sei a cor`;
+    }
+  };
+  
+  const checkTernary = (fruit === `maçã`) ? `Essa fruta é vermelha` 
+    : ((fruit === `banana`) ? `Esta fruta é amarela` : `Não sei a cor`);
+  
+  // Aninhar operadores  ternários, como no exemplo acima, não é uma boa prática,
+  // pois torna o seu código truncado e difícil de ler, tirando todo o sentido de um
+  // operador cujo objetivo é facilitar a sua vida e a da pessoa que lerá seu código
+  // no futuro!
