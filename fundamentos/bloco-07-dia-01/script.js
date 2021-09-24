bcf8fab8294fd4c158bb856b4e0b3c23e2fb9d15
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-24 15:25:19 -0300

    inicio arrow function

--- fundamentos/bloco-07-dia-01/script.js
+++ fundamentos/bloco-07-dia-01/script.js
@@ -1,8 +1,27 @@
-// Com o template literals
-console.log(`Primeira linha;
-Segunda linha;
-Terceira linha;`
-)
+/* função atual */
+function printName() {
+    const myName = 'Tiago'
+    return myName
+}
+console.log(printName());
 
-// Sem o template literals:
-console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n')
+/* usando arrow function */
+const printName = () => {
+    const myName = 'Tiago';
+    return myName;
+  }
+  console.log(printName());
+
+  /* Quando não há nada no corpo da função além do que será retornado, a sintaxe da arrow function nos permite simplificar ainda mais a função printName omitindo o return e as chaves: */
+  const printName = () => "Lucas";
+console.log(printName());
+
+/* Mas lembre-se que podemos omitir os parênteses apenas em um cenário:
+Quando a função recebe apenas um argumento, como podemos ver no exemplo abaixo: */
+const multiplyByTwo = number => number * 2;
+console.log(multiplyByTwo(10));
+
+/* Em funções que recebem mais de um parâmetro, os parênteses não são omitidos: */
+
+const multiplication = (number, multi) => number * multi;
+console.log(multiplication(8, 2));
