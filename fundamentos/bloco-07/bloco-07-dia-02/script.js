dd5dc4e8656414db32dd795d0304fde84c67418b
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-27 16:26:26 -0300

    exercicio 1 e 2

---
+++ fundamentos/bloco-07/bloco-07-dia-02/script.js
@@ -0,0 +1,50 @@
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+  const customerInfo = (order) => {
+     console.log(`Ola, ${order.name}, entrega para: ${order.name}, Telefone: ${order.phoneNumber},R. ${order.address.street}, N°:${order.address.number}, AP:${order.address.apartment}`);
+  }
+  
+  customerInfo(order);
+  
+  const orderModifier = (order) => {
+   order.name= 'Tiago Abranges';
+   order.payment.total = 50;
+   const pizzas = Object.keys(order.order.pizza)
+   console.log('-------------');
+  console.log(`Olá ${order.name}, o total do seu pedido de ${pizzas[0]},${pizzas[1]} e Coca-cola è R$ ${order.payment.total}`)
+  }
+  orderModifier(order);
