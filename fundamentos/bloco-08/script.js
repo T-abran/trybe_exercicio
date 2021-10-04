649dce9825a436996bc6863ac5c71556fb136bc1
Tiago Abranges <tiagoabranges@gmail.com>
2021-10-04 18:38:13 -0300

    aprendendo HOF

--- fundamentos/bloco-08/script.js
+++ fundamentos/bloco-08/script.js
@@ -1,21 +1,15 @@
-const despertador = () => {return 'Acordando!!'};
-
-const cafe = () => {return 'Bora tomar café!!'};
-
-const mimi = () => {return 'Partiu dormir!!'}
-
-const doingThings = (nomeDaFunçao) =>{
-     return nomeDaFunçao()
-}
-// Ao chamar a função doingThings:
-console.log(doingThings(despertador));
-
-// Ela deve retornar o valor do respectivo parâmetro, neste caso:
-// Acordando!!
-
-console.log(' ');
-console.log(despertador());
-console.log(' ');
-console.log(cafe());
-console.log(' ');
-console.log(mimi());
+const employeeGenerator = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
+    return { fullName, email: `${email}@trybe.com` };
+  };
+  
+  const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'),
+      id2: callback('Luiza Drumond'),
+      id3: callback('Carla Paiva'),
+    };
+    return employees;
+  };
+  
+  console.log(newEmployees(employeeGenerator));
