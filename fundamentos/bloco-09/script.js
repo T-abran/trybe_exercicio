cc045190fbcea403c097d291096939b74a05b4de
Tiago Abranges <tiagoabranges@gmail.com>
2021-10-18 17:17:35 -0300

    exercicio 3

--- fundamentos/bloco-09/script.js
+++ fundamentos/bloco-09/script.js
@@ -1,14 +1,31 @@
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
 
-const getUser = (callback) => {
-  const userToReturn = {
-    firstName: "Ivan",
-    lastName: "Ivanovich",
-    nationality: "Russian"
-  };
-  return callback(userToReturn);
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, callback) => {
+  setTimeout(()=> {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    }
+    else {
+      const errorMessage = "Country could not be found";
+      callback(errorMessage)
+    }
+  }, delay());
+
 };
 
-console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
-console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage);
