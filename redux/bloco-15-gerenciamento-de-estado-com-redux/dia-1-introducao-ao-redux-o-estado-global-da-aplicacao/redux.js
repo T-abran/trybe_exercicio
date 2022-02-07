752c997740726dada59ee193545380e4cd13b3e6
Tiago Abranges <tiagoabranges@gmail.com>
2022-02-07 17:27:07 -0300

    add exercice

--- redux/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/redux.js
+++ redux/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/redux.js
@@ -1,8 +1,18 @@
 const Redux = require('redux');
 
-const store = Redux.createStore();
-
 const ESTADO_INICIAL = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
+
+const reducer = (state = ESTADO_INICIAL) => {
+  switch (state){
+  case true:
+  return {
+  state
+  default
+}
+};
+
+
+const store = Redux.createStore(reducer);
