888061f63fa1ea109e242d6ab50122b026b63d6f
Tiago Abranges <tiagoabranges@gmail.com>
2022-05-10 17:00:44 -0300

    tentando exercicio

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -10,19 +10,44 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
-app.post('/recipes',
-function (req, res, next) {
+// ...
+function validateName(req, res, next) {
   const { name } = req.body;
-  if (!name || name === '') return res.status(400).json({ message: 'Invalid data!'}); // 1
+  if (!name || name === '') return res.status(400).json({ message: 'Invalid data!'});
+ console.log('cheguei validate');
+  next();
+   console.log('sai validate');
+};
 
-  next(); // 2
-},
-function (req, res) { // 3
+function validatePrice(req, res, next) {
+  const {price} = req.body;
+  if (!price || price !== typeof Number || price < 0) {
+    return res.status(400).json({ message: 'Invalid number!'})
+  }
+  next();
+}
+
+app.post('/recipes', validateName, validatePrice, function (req, res) {
   const { id, name, price } = req.body;
+  console.log('cheguei recipes');
   recipes.push({ id, name, price});
   res.status(201).json({ message: 'Recipe created successfully!'});
-});
 
+console.log('acabei recipes');});
+
+app.put('/recipes/:id', validateName, function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipesIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipesIndex === -1)
+    return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipesIndex] = { ...recipes[recipesIndex], name, price };
+
+  res.status(204).end();
+});
+// ...
 app.listen(3000, () => {
   console.log('Aplicação ouvindo na porta 3000');
 });
