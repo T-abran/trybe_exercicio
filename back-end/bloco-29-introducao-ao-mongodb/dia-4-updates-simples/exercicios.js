a9b41f7b54e2aab56abae138e41fefbddfddeb58
Tiago Abranges <tiagoabranges@gmail.com>
2022-07-21 16:37:59 -0300

    :rocket: inicio de exercicios

--- back-end/bloco-29-introducao-ao-mongodb/dia-4-updates-simples/exercicios.js
+++ back-end/bloco-29-introducao-ao-mongodb/dia-4-updates-simples/exercicios.js
@@ -1,16 +1,26 @@
-{
+use filminhos;
+db.movies.drop();
+db.movies.find();
+
+db.movies.insertMany(
+  [{
+    "_id": 1,
   "title": "Batman",
   "category": [ "action", "adventure" ],
   "imdbRating": 7.6,
   "budget": 35
 },
 {
+
+    "_id": 2,
   "title": "Godzilla",
   "category": [ "action", "adventure", "sci-fi" ],
   "imdbRating": 6.6
 },
 {
+
+    "_id": 3,
   "title": "Home Alone",
   "category": [ "family", "comedy" ],
   "imdbRating": 7.4
-}
+}]);
